0eff3db107f300058df5ecde457fb1b3306f7596
John Melo <61789156+johnlmelo@users.noreply.github.com>
2024-07-27 21:57:05 -0300

    Update pages

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const Pages = require('./pages');
 const FactaBank = require('./banco_facta/routes');
 const Promosys = require('./promossis/routes');
-const Whatsapp = require('./whatsapp/routes');
+// const Whatsapp = require('./whatsapp/routes');
 
 const setupRoutes = (app) => {
     app.use('/', FactaBank);
     app.use('/', Promosys);
-    app.use('/', Whatsapp);
+    // app.use('/', Whatsapp);
     app.use('/', Pages);
 };
 
