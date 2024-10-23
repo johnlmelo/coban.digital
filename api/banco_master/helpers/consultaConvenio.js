4f130a5bbaa162a7ce326a52fb7d290d72fc3ea4
John Melo <61789156+johnlmelo@users.noreply.github.com>
2024-10-23 17:43:12 -0300

    Add: Consulta Convenio

---
+++ api/banco_master/helpers/consultaConvenio.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const AMBIENTE = process.env.AMBIENTE;
+const MASTER_URL = AMBIENTE === "prod" ? process.env.MASTER_URL_PROD : process.env.MASTER_URL_HOM;
+
+const obterConvenio = async (cpf, token) => {
+
+    
+    try {
+        
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `https://hml-api-parceiro.bancomaster.com.br/consignado/v1/cliente/consulta-cpf?cpfRequest=${cpf}`,
+            headers: { 
+                'Authorization': `Bearer ${token}`
+            }
+        };
+    
+        const response = await axios.request(config);
+
+        const data = response.data;
+    
+        return data;
+    } catch (error) {
+        console.error('Erro ao consultar o limite', error);
+        return error;
+    }
+};
+
+
+const consultaConvenio = async (req, res) =>  {
+    
+    const resposta = await obterConvenio(req.params.cpf, req.params.convenioId, req.body.token);
+    
+    const cadastros = {
+        res: resposta,
+        count: resposta.length
+    }
+    
+    return res.status(200).json(cadastros);
+    
+}
+
+module.exports = consultaConvenio;
--- api/banco_master/routes.js
+++ api/banco_master/routes.js
@@ -4,10 +4,13 @@ const app = express.Router();
 const GeraToken = require('./helpers/GeraToken'); 
 const VerificarStatus = require('./helpers/VerificarStatus');
 const consultarLimite = require('./helpers/consultarLimite');
+const consultaConvenio = require('./helpers/consultaConvenio');
 
 // Criar um card
 app.get('/master/gera-token', GeraToken, VerificarStatus);
 
 app.get('/master/limite/consultar/:cpf/:convenioId', GeraToken, consultarLimite);
+
+app.get('/master/convenio/consultar/:cpf', GeraToken, consultaConvenio);
 
 module.exports = app;
