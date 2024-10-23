779c197e0569dc32562bf680a55beb7c7d5629bf
John Melo <61789156+johnlmelo@users.noreply.github.com>
2024-10-23 17:47:25 -0300

    Add update

--- api/banco_master/helpers/consultarLimite.js
+++ api/banco_master/helpers/consultarLimite.js
@@ -5,41 +5,40 @@ const AMBIENTE = process.env.AMBIENTE;
 const MASTER_URL = AMBIENTE === "prod" ? process.env.MASTER_URL_PROD : process.env.MASTER_URL_HOM;
 
 const obterLimite = async (cpf, convenioId, token) => {
-    const url = `https://hml-api-parceiro.bancomaster.com.br/consignado/v1/limite/consultar/${cpf}/${convenioId}`;
-    const options = {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    };
 
+    
     try {
-        const response = await axios.get(url, options);
+        
+        let config = {
+            method: 'get',
+            maxBodyLength: Infinity,
+            url: `https://hml-api-parceiro.bancomaster.com.br/consignado/v1/limite/consultar/${cpf}/${convenioId}`,
+            headers: { 
+                'Authorization': `Bearer ${token}`
+            }
+        };
+    
+        const response = await axios.request(config);
 
         const data = response.data;
     
         return data;
     } catch (error) {
         console.error('Erro ao consultar o limite', error);
-        return 'Erro ao consultar o limite';
+        return error;
     }
 };
 
 
 const consultarLimite = async (req, res) =>  {
     
-    console.log("req.body.token", req.body.token);
-
     const resposta = await obterLimite(req.params.cpf, req.params.convenioId, req.body.token);
-
     
     const cadastros = {
         res: resposta,
         count: resposta.length
     }
     
-    console.log('resposta', cadastros)
-    // Verifica se o status da resposta é 200
     return res.status(200).json(cadastros);
     
 }
