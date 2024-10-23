303e9ba837ff1513ae1afa385e0b5de65b96e6f5
John Melo <61789156+johnlmelo@users.noreply.github.com>
2024-10-23 18:14:51 -0300

    Add nova matricula

---
+++ api/banco_master/helpers/tratarContasPorMatricula.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const AMBIENTE = process.env.AMBIENTE;
+const MASTER_URL = AMBIENTE === "prod" ? process.env.MASTER_URL_PROD : process.env.MASTER_URL_HOM;
+
+const tratarContasPorMatricula = async (req, res) =>  {
+    
+    const {contas, matricula} = req.body;
+
+    const filterMatricula = contas.find(item => item.matricula == matricula) || [];
+    
+    return res.status(200).json(filterMatricula);
+    
+}
+
+module.exports = tratarContasPorMatricula;
--- api/banco_master/routes.js
+++ api/banco_master/routes.js
@@ -5,6 +5,7 @@ const GeraToken = require('./helpers/GeraToken');
 const VerificarStatus = require('./helpers/VerificarStatus');
 const consultarLimite = require('./helpers/consultarLimite');
 const consultaConvenio = require('./helpers/consultaConvenio');
+const tratarContasPorMatricula = require('./helpers/tratarContasPorMatricula');
 
 // Criar um card
 app.get('/master/gera-token', GeraToken, VerificarStatus);
@@ -12,5 +13,7 @@ app.get('/master/gera-token', GeraToken, VerificarStatus);
 app.get('/master/limite/consultar/:cpf/:convenioId', GeraToken, consultarLimite);
 
 app.get('/master/convenio/consultar/:cpf', GeraToken, consultaConvenio);
+
+app.post('/master/contas/tratar-dados', GeraToken, tratarContasPorMatricula);
 
 module.exports = app;
